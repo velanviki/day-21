048756428a2e1902a618c35ec7a53c12ec85b5ff
velanviki <92292839+velanviki@users.noreply.github.com>
2021-11-26 19:43:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+setTimeout(function () {
+    var div = document.createElement("div");
+    div.style.fontSize = '250%'
+    div.style.marginLeft = "49.3%"
+    div.style.color = "orange";
+    div.innerText = "10"
+    document.body.appendChild(div);
+    console.log("10");
+    setTimeout(function () {
+        var div = document.createElement("div");
+        div.style.fontSize = '250%';
+        div.style.marginLeft = "50%"
+        div.style.color = "orange";
+        div.innerText = "9"
+        document.body.appendChild(div);
+        console.log("9");
+        setTimeout(function () {
+            var div = document.createElement("div");
+            div.style.fontSize = '250%';
+            div.style.marginLeft = "50%";
+            div.style.color = "orange";
+            div.innerText = "8"
+            document.body.appendChild(div);
+            console.log("8");
+            setTimeout(function () {
+                var div = document.createElement("div");
+                div.style.fontSize = '250%';
+                div.style.marginLeft = "50%";
+                div.style.color = "orange";
+                div.innerText = "7"
+                document.body.appendChild(div);
+                console.log("7");
+                setTimeout(function () {
+                    var div = document.createElement("div");
+                    div.style.fontSize = '250%';
+                    div.style.marginLeft = "50%";
+                    div.style.color = "gray";
+                    div.innerText = "6"
+                    document.body.appendChild(div);
+                    console.log("6");
+                    setTimeout(function () {
+                        var div = document.createElement("div");
+                        div.style.fontSize = '250%';
+                        div.style.marginLeft = "50%";
+                        div.style.color = "gray";
+                        div.innerText = "5"
+                        document.body.appendChild(div);
+                        console.log("5");
+                        setTimeout(function () {
+                            var div = document.createElement("div");
+                            div.style.fontSize = '250%';
+                            div.style.marginLeft = "50%";
+                            div.style.color = "gray";
+                            div.innerText = "4"
+                            document.body.appendChild(div);
+                            console.log("4");
+                            setTimeout(function () {
+                                var div = document.createElement("div");
+                                div.style.fontSize = '250%';
+                                div.style.marginLeft = "50%";
+                                div.style.color = "green";
+                                div.innerText = "3"
+                                document.body.appendChild(div);
+                                console.log("3");
+                                setTimeout(function () {
+                                    var div = document.createElement("div");
+                                    div.style.fontSize = '250%';
+                                    div.style.marginLeft = "50%";
+                                    div.style.color = "green";
+                                    div.innerText = "2"
+                                    document.body.appendChild(div);
+                                    console.log("2");
+                                    setTimeout(function () {
+                                        var div = document.createElement("div");
+                                        div.style.fontSize = '250%';
+                                        div.style.marginLeft = "50%";
+                                        div.style.color = "green";
+                                        div.innerText = "1"
+                                        document.body.appendChild(div);
+                                        console.log("1");
+                                        setTimeout(function () {
+                                            var div = document.createElement("div");
+                                            div.style.fontSize = '250%';
+                                            div.style.marginLeft = "35%";
+                                            div.innerText = "Happy Independence Day!!!";
+                                            document.body.appendChild(div);
+
+                                            console.log("Happy Independence Day!!!");
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+
+}, 1000)
+
